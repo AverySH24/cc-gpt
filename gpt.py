0bcad28798ee36b6a92a4e6071d89f5fe855ca51
AverySH24 <AverySH@comcast.net>
2023-04-14 15:54:44 -0500

    remove excess

--- gpt.py
+++ gpt.py
@@ -2,13 +2,11 @@ import http.client
 import json
 import config
 
-# DO NOT PUSH TO GITHUB WITH APIKEY I WILL KILL YOU!!!!
 conn = http.client.HTTPSConnection(config.http_connection)
 
 # query is what is google?
 payload = "{\r\n    \"query\": \"What is google?\"\r\n}"
 
-# DO NOT PUSH TO GITHUB WITH APIKEY I WILL KILL YOU!!!!
 headers = {
     'content-type': "application/json",
     'X-RapidAPI-Key': config.api_key,
@@ -22,7 +20,6 @@ res = conn.getresponse()
 data = res.read().decode("utf-8")
 json_data = json.loads(data)
 
-# DO NOT PUSH TO GITHUB WITH APIKEY I WILL KILL YOU!!!!
 print("Returned Message: \n")
 print(json_data["response"] + " -- This is the chatgpt response!\n")
 print(json_data["conversationId"] + " -- This is the conversation id!\n")
@@ -33,4 +30,3 @@ print("Hi Avery! Did this work?")
 #   "conversationId": "819e6c3e-e7cb-466c-83af-b0d137b93528",
 #   "response": "Google is a multinational technology company specializing in Internet-related services and products, including online advertising technologies, search engine, cloud computing, software, and hardware."
 # }
-# DO NOT PUSH TO GITHUB WITH APIKEY I WILL KILL YOU!!!!
