31fb6a01b3eafd8ea3725ed6082f80078404b386
Avery <AverySH@comcast.net>
2023-04-20 12:14:24 -0500

    now

--- gpt.py
+++ gpt.py
@@ -5,7 +5,7 @@ import config
 conn = http.client.HTTPSConnection(config.http_connection)
 
 # query is what is google?
-payload = "{\r\n    \"query\": \"What is google?\"\r\n}"
+
 
 headers = {
     'content-type': "application/json",
@@ -13,18 +13,22 @@ headers = {
     'X-RapidAPI-Host': config.host
     }
 
-conn.request("POST", "/ask", payload, headers)
 
-res = conn.getresponse()
-# data serves as a json object in this case
-data = res.read().decode("utf-8")
-json_data = json.loads(data)
+# this should return a json object with the result and the conversationID
+# print("Returned Message: \n")
+# print(json_data["response"] + " -- This is the chatgpt response!\n")
+# print(json_data["conversationId"] + " -- This is the conversation id!\n")
 
-print("Returned Message: \n")
-print(json_data["response"] + " -- This is the chatgpt response!\n")
-print(json_data["conversationId"] + " -- This is the conversation id!\n")
+def make_query(query, conversation):
+    payload = "{\r\n    \"query\": \"" + query + "\"\r\n}"
+    conn.request("POST", "/ask", payload, headers)
+    res = conn.getresponse()
+    # data serves as a json object in this case
+    data = res.read().decode("utf-8")
+    json_data = json.loads(data)
 
-print("Hi Avery! Did this work?")
+def start_convo(name):
+    payload = "{\r\n    \"query\": \"Respond to all my prompts as if you were " + name + ". Greet me as if you were the composer.\"\r\n}"
 
 # {
 #   "conversationId": "819e6c3e-e7cb-466c-83af-b0d137b93528",
--- gui.py
+++ gui.py
@@ -2,20 +2,6 @@
 # https://www.youtube.com/watch?v=iM3kjbbKHQU
 
 # https://www.activestate.com/resources/datasheets/tkinter-cheatsheet-tips-and-tricks-to-create-your-user-interface/
-# import tkinter
-# import customtkinter
-# from tkinter import *
-
-# customtkinter.set_appearance_mode("dark")
-# customtkinter.set_default_color_theme("dark-blue")
-
-# root = customtkinter.CTk()
-# root.geometry("500x350")
-
-
-# frame = customtkinter.CtkFrame(master = root)
-
-# root.mainloop()
 
 import tkinter
 import customtkinter as customtkinter
@@ -23,23 +9,107 @@ import customtkinter as customtkinter
 # print("Hello")
 
 class App(customtkinter.CTk):
-   def __init__(self):
-    super().__init__()
 
-    customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
-    customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+    def __init__(self, *args, **kwargs):
+        customtkinter.CTk.__init__(self, *args, **kwargs)
 
-    app = customtkinter.CTk()  # create CTk window like you do with the Tk window
-    app.geometry("700x540")
+        # the container is where we'll stack a bunch of frames
+        # on top of each other, then the one we want visible
+        # will be raised above the others
+        container = customtkinter.CTkFrame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
-    def button_function():
-        print("button pressed")
+        self.frames = {}
+        for F in (StartPage, ChatPage):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
 
-    # Use CTkButton instead of tkinter Button
-    button = customtkinter.CTkButton(master=app, text="CTkButton", command=button_function)
-    button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
+            # put all of the pages in the same location;
+            # the one on the top of the stacking order
+            # will be the one that is visible.
+            frame.grid(row=0, column=0, sticky="nsew")
 
-# if __name__ == "__main__":
+        self.show_frame("StartPage")
+
+    def show_frame(self, page_name):
+        '''Show a frame for the given page name'''
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+
+class StartPage(customtkinter.CTkFrame):
+
+    def __init__(self, parent, controller):
+        
+        def button_function(composer):
+            print(composer)
+            controller.show_frame("ChatPage")
+
+
+        customtkinter.CTkFrame.__init__(self, parent)
+        self.controller = controller
+
+            # Use CTkButton instead of tkinter Button
+        buttonBeethoven = customtkinter.CTkButton(self, text="Beethoven", command= lambda: button_function("beethoven"))
+        buttonBeethoven.place(relx=0.25, rely=0.25, anchor=tkinter.CENTER)
+
+        buttonMozart = customtkinter.CTkButton(self, text="Mozart", command=lambda: button_function("mozart"))
+        buttonMozart.place(relx=0.75, rely=0.25, anchor=tkinter.CENTER)
+
+        buttonBach = customtkinter.CTkButton(self, text="Bach", command=lambda: button_function("bach"))
+        buttonBach.place(relx=0.25, rely=0.5, anchor=tkinter.CENTER)
+
+        buttonBrahms = customtkinter.CTkButton(self, text="Brahms", command=lambda: button_function("brahms"))
+        buttonBrahms.place(relx=0.75, rely=0.5, anchor=tkinter.CENTER)
+
+        buttonTchaikovsky = customtkinter.CTkButton(self, text="Tchaikovsky", command=lambda: button_function("tchaikovsky"))
+        buttonTchaikovsky.place(relx=0.25, rely=0.75, anchor=tkinter.CENTER)
+
+        buttonChopin = customtkinter.CTkButton(self, text="Chopin", command=lambda: button_function("chopin"))
+        buttonChopin.place(relx=0.75, rely=0.75, anchor=tkinter.CENTER)
+    # customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
+    # customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+
+    # app = customtkinter.CTk()  # create CTk window like you do with the Tk window
+    # app.geometry("700x540")
+
+
+
+    
+
+# CHANGE TO FRAME LATER
+class ChatPage(customtkinter.CTkFrame):
+    def __init__(self, parent, controller):
+        customtkinter.CTkFrame.__init__(self, parent)
+        self.controller = controller
+
+        def new_entry():
+            textbox.insert("end", "You: " + entry.get() + "\n")
+            textbox.insert("end", "Beethoven: " + entry.get() + "\n")
+
+
+
+        entry = customtkinter.CTkEntry(self, fg_color = "transparent", border_width = 2, text_color = ("gray10", "#DCE4EE"))
+        entry.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
+
+        main_button_1 = customtkinter.CTkButton(self, fg_color="transparent", border_width=2, text = "Send", text_color=("gray10", "#DCE4EE"), command = new_entry)
+        main_button_1.grid(row=3, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
+
+        textbox = customtkinter.CTkTextbox(self, width=250)
+        textbox.grid(row=0, column=1, padx=(20, 0), pady=(20, 0), sticky="nsew")
+
+        home_button = customtkinter.CTkButton(self, fg_color="transparent", border_width=2, text = "Back", text_color=("gray10", "#DCE4EE"), command = lambda: controller.show_frame("StartPage"))
+        home_button.grid(row=2, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
+    
+
+        # customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
+        # customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
+
+        # app = customtkinter.CTk()  # create CTk window like you do with the Tk window
+        # app.geometry("700x540")
+
 app = App()
 app.mainloop()
-print("Goodbye")
