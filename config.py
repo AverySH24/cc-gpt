00e7857b9386d314f0f990ea04086e758564a486
AverySH24 <AverySH@comcast.net>
2023-04-21 09:44:44 -0500

    multiple pages

---
+++ config.py
@@ -0,0 +1,3 @@
+api_key = "873f6481bamsh81655841b214089p174644jsne29b149a6b740"
+host = "chatgpt-ai-chat-bot.p.rapidapi.com"
+http_connection = "chatgpt-ai-chat-bot.p.rapidapi.com"
--- gui.py
+++ gui.py
@@ -24,8 +24,13 @@ class App(customtkinter.CTk):
         self.frames = {}
         for F in (StartPage, ChatPage):
             page_name = F.__name__
-            frame = F(parent=container, controller=self)
-            self.frames[page_name] = frame
+            if F is ChatPage:
+                for name in ("Beethoven", "Mozart", "Brahms", "Bach", "Tchaikovsky", "Chopin"):
+                    frame = F(parent=container, controller=self, page = name)
+                    self.frames[page_name + name] = frame
+            else:
+                frame = F(parent=container, controller=self, page = "start")
+                self.frames[page_name] = frame
 
             # put all of the pages in the same location;
             # the one on the top of the stacking order
@@ -46,51 +51,48 @@ class StartPage(customtkinter.CTkFrame):
         
         def button_function(composer):
             print(composer)
-            controller.show_frame("ChatPage")
+            controller.show_frame("ChatPage" + composer)
 
 
         customtkinter.CTkFrame.__init__(self, parent)
         self.controller = controller
 
             # Use CTkButton instead of tkinter Button
-        buttonBeethoven = customtkinter.CTkButton(self, text="Beethoven", command= lambda: button_function("beethoven"))
+        buttonBeethoven = customtkinter.CTkButton(self, text="Beethoven", command= lambda: button_function("Beethoven"))
         buttonBeethoven.place(relx=0.25, rely=0.25, anchor=tkinter.CENTER)
 
-        buttonMozart = customtkinter.CTkButton(self, text="Mozart", command=lambda: button_function("mozart"))
+        buttonMozart = customtkinter.CTkButton(self, text="Mozart", command=lambda: button_function("Mozart"))
         buttonMozart.place(relx=0.75, rely=0.25, anchor=tkinter.CENTER)
 
-        buttonBach = customtkinter.CTkButton(self, text="Bach", command=lambda: button_function("bach"))
+        buttonBach = customtkinter.CTkButton(self, text="Bach", command=lambda: button_function("Bach"))
         buttonBach.place(relx=0.25, rely=0.5, anchor=tkinter.CENTER)
 
-        buttonBrahms = customtkinter.CTkButton(self, text="Brahms", command=lambda: button_function("brahms"))
+        buttonBrahms = customtkinter.CTkButton(self, text="Brahms", command=lambda: button_function("Brahms"))
         buttonBrahms.place(relx=0.75, rely=0.5, anchor=tkinter.CENTER)
 
-        buttonTchaikovsky = customtkinter.CTkButton(self, text="Tchaikovsky", command=lambda: button_function("tchaikovsky"))
+        buttonTchaikovsky = customtkinter.CTkButton(self, text="Tchaikovsky", command=lambda: button_function("Tchaikovsky"))
         buttonTchaikovsky.place(relx=0.25, rely=0.75, anchor=tkinter.CENTER)
 
-        buttonChopin = customtkinter.CTkButton(self, text="Chopin", command=lambda: button_function("chopin"))
+        buttonChopin = customtkinter.CTkButton(self, text="Chopin", command=lambda: button_function("Chopin"))
         buttonChopin.place(relx=0.75, rely=0.75, anchor=tkinter.CENTER)
-    # customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
-    # customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
-
-    # app = customtkinter.CTk()  # create CTk window like you do with the Tk window
-    # app.geometry("700x540")
-
 
 
     
 
 # CHANGE TO FRAME LATER
 class ChatPage(customtkinter.CTkFrame):
-    def __init__(self, parent, controller):
+    def __init__(self, parent, controller, name):
         customtkinter.CTkFrame.__init__(self, parent)
         self.controller = controller
 
+        convo = 0
+        if (not convo):
+            # there is a new page, you need to create a new conversation with a new prompt.
+            convo += 1
+
         def new_entry():
             textbox.insert("end", "You: " + entry.get() + "\n")
-            textbox.insert("end", "Beethoven: " + entry.get() + "\n")
-
-
+            textbox.insert("end", name + ":" + entry.get() + "\n")
 
         entry = customtkinter.CTkEntry(self, fg_color = "transparent", border_width = 2, text_color = ("gray10", "#DCE4EE"))
         entry.grid(row=3, column=1, columnspan=2, padx=(20, 0), pady=(20, 20), sticky="nsew")
@@ -103,13 +105,7 @@ class ChatPage(customtkinter.CTkFrame):
 
         home_button = customtkinter.CTkButton(self, fg_color="transparent", border_width=2, text = "Back", text_color=("gray10", "#DCE4EE"), command = lambda: controller.show_frame("StartPage"))
         home_button.grid(row=2, column=3, padx=(20, 20), pady=(20, 20), sticky="nsew")
-    
 
-        # customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
-        # customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
-
-        # app = customtkinter.CTk()  # create CTk window like you do with the Tk window
-        # app.geometry("700x540")
 
 app = App()
 app.mainloop()
